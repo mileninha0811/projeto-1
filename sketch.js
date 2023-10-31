a6aa821d1debb85fface6b598e0da058bf5716c2
mileninha0811 <148898139+mileninha0811@users.noreply.github.com>
2023-10-31 13:52:09 -0300

    adicionando os arquivos html.js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+   background("pink");
+    ;}
+  function draw() {
+    stroke("white")
+    fill("cyan")
+    
+    if(mouseIsPressed)
+      rect(mouseX,mouseY,20,20);}
+  
+  
+  
+    
